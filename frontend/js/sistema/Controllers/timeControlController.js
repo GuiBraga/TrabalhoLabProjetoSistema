9f1b7bdfbe348225523ef2302d20f3d459eec93c
Paula Magalhães <pmagalhaesa@hotmail.com>
2017-03-06 00:08:06 -0300

    Pagina inicial do site e inicial do sistema

---
+++ frontend/js/sistema/Controllers/timeControlController.js
@@ -0,0 +1,1 @@
+angular.module("timeControl").controller("timeControlController", function($scope){ });
---
+++ frontend/js/site/Config/routeConfig.js
@@ -0,0 +1,17 @@
+angular.module("timeControl").config(function($routeProvider){
+    $routeProvider.when("/home", {
+		templateUrl: "layout/site/home.html",
+		controller: "timeControlController",
+    });
+
+     $routeProvider.when("/funcionalidades", {
+		templateUrl: "layout/site/funcionalidades.html",
+		controller: "timeControlController",
+    });
+
+    $routeProvider.when("/login", {
+		templateUrl: "layout/site/login.html",
+		controller: "timeControlController",
+    });
+    $routeProvider.otherwise({redirectTo: "/home"});
+});
