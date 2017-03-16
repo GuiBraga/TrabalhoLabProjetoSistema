9a92163f7ee8f588ee5b530107795f3c0f3d60e9
Daiane <dai.daianeaparecidaoliveira@gmail.com>
2017-03-15 23:32:29 -0300

    Adição telas Login e Cadastro

--- frontend/js/sistema/Config/routeConfig.js
+++ frontend/js/sistema/Config/routeConfig.js
@@ -1,12 +1,18 @@
 angular.module("timeControl").config(function($routeProvider){
     $routeProvider.when("/inicio", {
 		templateUrl: "layout/sistema/inicio.html",
-		controller: "timeControlController",
+		controller: "homeController",
     });
 
  $routeProvider.when("/atividades", {
 		templateUrl: "layout/sistema/atividades/lista-atividade.html",
-		controller: "timeControlController",
+		controller: "atividadeController",
     });
-    $routeProvider.otherwise({redirectTo: "/atividades"});
+
+$routeProvider.when("/tempo_investido", {
+		templateUrl: "layout/sistema/tempo_investido/lista-tempo-investido.html",
+		controller: "tempoInvestidoController",
+    });
+
+    $routeProvider.otherwise({redirectTo: "/inicio"});
 });
