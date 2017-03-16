519f3dcb02f484396d441c3c4d6fbc1e3cb41f50
Daiane <dai.daianeaparecidaoliveira@gmail.com>
2017-03-15 23:37:00 -0300

    Login e Cadastro
    Adição das telas de login e cadastro integradas ao restante do sistema.
    (parte front apenas)

---
+++ frontend/js/sistema/Controllers/atividadeController.js
@@ -0,0 +1,27 @@
+angular.module("timeControl").controller("atividadeController", function($scope){
+
+  $scope.atividades=[
+    {
+    "id": 1,
+    "nome-atividade": "fazer sistema",
+    "tempo-gasto-atividade": "10:00:00",
+    "prioridade-atividade": "0",
+    "observacao-atividade": "teste teste teste teste teste teste "
+  },
+  {
+    "id": 2,
+    "nome-atividade": "fazer sistema",
+    "tempo-gasto-atividade": "10:00:00",
+    "prioridade-atividade": "0",
+    "observacao-atividade": "teste teste teste teste teste teste "
+  },
+  {
+    "id": 3,
+    "nome-atividade": "fazer sistema",
+    "tempo-gasto-atividade": "10:00:00",
+    "prioridade-atividade": "0",
+    "observacao-atividade": "teste teste teste teste teste teste "
+    }
+  ]
+
+ });
---
+++ frontend/js/sistema/Controllers/homeController.js
@@ -0,0 +1,58 @@
+angular.module("timeControl").controller("homeController", function($scope){
+  
+  $("#calendar").fullCalendar({
+			locale: "pt-br",
+			defaultView: 'agendaWeek',
+			showMinutes: true,
+			showAllDay: false,
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'agendaWeek,agendaDay'
+			},
+			defaultDate: new Date(),
+			navLinks: true, 
+			selectable: true,
+			selectHelper: true,
+			select: function(start, end) {
+				$("#modal-nova-atividade").modal("show");
+
+				// var title = prompt('Event Title:');
+				// var eventData;
+				// if (title) {
+				// 	eventData = {
+				// 		title: title,
+				// 		start: start,
+				// 		end: end
+				// 	};
+				// 	$('#calendar').fullCalendar('renderEvent', eventData, true); 
+				// }
+				// $('#calendar').fullCalendar('unselect');
+			},
+			editable: false,
+			eventLimit: true,
+			events: [
+				{
+					title: 'teste',
+					start: '2017-03-12T09:00:00',
+					end: '2017-03-12T10:00:00',
+					color: '#34b04f'
+				},
+				{
+					title: 'teste',
+					start: '2017-03-12T10:00:00',
+					end: '2017-03-12T10:31:00',
+					color: '#f8e64a'
+				},
+				{
+					title: 'teste',
+					start: '2017-03-12T08:00:00',
+					end: '2017-03-12T08:20:00',
+					color: '#f44336'
+				}
+			]
+		});
+
+    $(".fc-time-grid-container").height("498px");
+
+ });
