4b93db986dc5ebde5de5f522a369b90e6b81d438
Guilherme <gui-gbo@hotmail.com>
2017-04-16 12:15:08 -0300

    implementação js login
    implementado integração do JS com controller do java

--- projeto/time-control-web/WebContent/js/site/Controllers/usuarioController.js
+++ projeto/time-control-web/WebContent/js/site/Controllers/usuarioController.js
@@ -1,5 +1,17 @@
-angular.module("timeControl").controller("usuarioController", function($scope){
+angular.module("timeControl").controller("usuarioController", ['$scope', '$http', function($scope, $http) {
 
 	$('.menu-topo').addClass('esconde');
 	$('.logo').addClass('usuario');
-});
+
+	$scope.logar = function () {
+		$http({
+			method : "GET",
+			url : "http://localhost:8080/time-control/usuario/validaLogin",
+			params : {"email" : $scope.email, "senha": $scope.senha}
+		}).then(function mySucces(response) {
+			alert("Logado com sucesso!");
+		}, function myError(response) {
+			alert("Senha ou usuário incorretos!");
+		});
+	}
+}]);
