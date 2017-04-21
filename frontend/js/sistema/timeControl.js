5ed1766ddc4004962fd0daf739c49bf2d954387d
Guilherme <gui-gbo@hotmail.com>
2017-04-21 18:31:26 -0300

    Implemetação login e cadastro
    Login e cadastro OK, retirado pasta frontEnd

--- frontend/js/sistema/timeControl.js
+++
@@ -1,1 +0,0 @@
-angular.module("timeControl", ["ngMessages", "ngRoute", "googlechart"]);
--- frontend/js/site/Controllers/timeControlController.js
+++
@@ -1,6 +0,0 @@
-angular.module("timeControl").controller("timeControlController", function($scope){
-	$('header').removeClass('header-page');
-
-	$('.menu-topo').removeClass('esconde');
-	$('.logo').removeClass('usuario');
-});
